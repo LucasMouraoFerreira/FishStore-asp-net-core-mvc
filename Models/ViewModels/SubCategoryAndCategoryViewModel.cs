4722114bba665a2ab5faa81d11ec2f9051447d70
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2019-12-12 19:30:08 -0300

    CRUD sub categorias

---
+++ Models/ViewModels/SubCategoryAndCategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishStore.Models.ViewModels
+{
+    public class SubCategoryAndCategoryViewModel
+    {
+        public IEnumerable<Category> CategoryList { get; set; }
+        public SubCategory SubCategory { get; set; }
+        public List<string> SubCategoryList { get; set; }
+        public string StatusMessage { get; set; }
+
+
+    }
+}
